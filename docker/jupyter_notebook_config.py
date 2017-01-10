18b7c55c764cb5f4013274a2b8206695b782e3a0
SebastianCa <cammerer@inue.uni-stuttgart.de>
2017-01-10 17:22:04 +0100

    Update jupyter_notebook_config.py

--- docker/jupyter_notebook_config.py
+++ docker/jupyter_notebook_config.py
@@ -17,3 +17,5 @@ c.NotebookApp.ip = '*'
 c.NotebookApp.port = 8888
 c.NotebookApp.open_browser = False
 c.MultiKernelManager.default_kernel_name = 'python2'
+c.NotebookApp.token = ''
+c.NotebookApp.password = ''
